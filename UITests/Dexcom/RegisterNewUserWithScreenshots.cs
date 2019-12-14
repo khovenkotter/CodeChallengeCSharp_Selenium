a8a0ee0eb38a49bba2cb30da2af5ec8b4e0b03ab
khovenkotter <khovenkotter@users.noreply.github.com>
2019-12-14 15:22:03 -0800

    02ProjectComplete
    Adds page objects
    adds test cases
    All tests pass

--- UITests/Dexcom/RegisterNewUserWithScreenshots.cs
+++ UITests/Dexcom/RegisterNewUserWithScreenshots.cs
@@ -11,20 +11,259 @@ namespace DexcomCodeChallenge.UITests.Dexcom
     [TestFixture]
     public class RegisterNewUserWithScreenshots
     {
+        //This test requires the directory dir to exist in order to successfully save screenshots
         IWebDriver driver;
+        string timestamp = DateTime.Now.ToString("yyyyMMddhhmmss");
+        string dir = "C:/temp/screenshots/";
+        string url = "https://shrouded-lowlands-66853.herokuapp.com/";
+        string fname = "firstname";
+        string fname16 = "firstnameabcdefg";
+        string lname = "lastname";
+        string title = "benevolent dictator for life";
+        string titleNumbers = "benevolent dictator for 99 years";
+        string email = "karl.testallthethings@gmail.com";
+        string phone = "5035551212";
+        string bus = "testing things";
+        string add1 = "123 main st";
+        string add2 = "suite 42";
+        string city = "Portland";
+        string zip = "97001";
+        string zip4 = "9700";
+        string zip24 = "970012345678901234567890";
+
 
         [Test]
         [Category("UI")]
-        public void NavigateToTestPage()
+        public void A01thruA05()
         {
             driver = new ChromeDriver();
-            
+            //A01
+            driver.Url = url;
+            TakeScreenshot(driver, "A01", timestamp);
+
+            //A02
+            DexcomTestPage page = new DexcomTestPage(driver);
+            page.TypeLastName(lname);
+            page.TypeSuffix("MD");
+            page.TypeTitle(title);
+            page.TypeEmail(email);
+            page.TypePhone(phone);
+            page.TypeBusiness(bus);
+            page.TypeAddressLn1(add1);
+            page.TypeCity(city);
+            page.TypeState("OR");
+            page.TypeZip(zip);
+            page.TermsBox.Click();
+            page.PrivacyBox.Click();
+            page.SaveBtn.Click();
+            TakeScreenshot(driver, "A02", timestamp);
+
+            //A03
+            page.ClearAllFields();
+            page.TypeLastName(lname);
+            page.TypeSuffix("MD");
+            page.TypeTitle(title);
+            page.TypeEmail(email);
+            page.TypePhone(phone);
+            page.TypeBusiness(bus);
+            page.TypeAddressLn1(add1);
+            page.TypeCity(city);
+            page.TypeState("OR");
+            page.TypeZip(zip);
+            page.TermsBox.Click();
+            page.PrivacyBox.Click();
+            page.TypeFirstName(fname16);
+            TakeScreenshot(driver, "A03", timestamp);
+            Console.WriteLine("Typed FirstName as " +fname16 );
+            var recordedName = page.FirstName.GetAttribute("value");
+            Console.WriteLine("Recorded FirsName as " + recordedName);
+
+            //A04
+            page.ClearAllFields();
+            page.TypeFirstName(fname);
+            page.TypeLastName(lname);
+            page.TypeTitle(title);
+            page.TypeEmail(email);
+            page.TypePhone(phone);
+            page.TypeBusiness(bus);
+            page.TypeAddressLn1(add1);
+            page.TypeCity(city);
+            page.TypeState("OR");
+            page.TypeZip(zip);
+            page.TermsBox.Click();
+            page.PrivacyBox.Click();
+            TakeScreenshot(driver, "A04", timestamp);
+
+            //A05
+            page.SaveBtn.Click();
+            TakeScreenshot(driver, "A05", timestamp);
+            driver.Close();
         }
-        protected void TakeScreenshot(IWebDriver driver, string stepName)
+
+        [Test]
+        [Category("UI")]
+        public void A06thruA08()
+        {
+            driver = new ChromeDriver();
+            //A06
+            driver.Url = url;
+
+            //A07
+            DexcomTestPage page = new DexcomTestPage(driver);
+            page.ClearAllFields();
+            page.TypeFirstName(fname);
+            page.TypeLastName(lname);
+            page.TypeTitle(titleNumbers);
+            page.TypeEmail(email);
+            page.TypePhone(phone);
+            page.TypeBusiness(bus);
+            page.TypeAddressLn1(add1);
+            page.TypeCity(city);
+            page.TypeState("OR");
+            page.TypeZip(zip);
+            page.TermsBox.Click();
+            page.PrivacyBox.Click();
+            page.SaveBtn.Click();
+            TakeScreenshot(driver, "A07", timestamp);
+
+            //A08
+            page.ClearAllFields();
+            page.TypeFirstName(fname);
+            page.TypeLastName(lname);
+            page.TypeEmail(email);
+            page.TypePhone(phone);
+            page.TypeBusiness(bus);
+            page.TypeAddressLn1(add1);
+            page.TypeCity(city);
+            page.TypeState("OR");
+            page.TypeZip(zip);
+            page.TermsBox.Click();
+            page.PrivacyBox.Click();
+            page.TypeTitle(title);
+            page.SaveBtn.Click();
+            TakeScreenshot(driver, "A08", timestamp);
+            driver.Close();
+           
+        }
+
+        [Test]
+        [Category("UI")]
+        public void A09thruA12()
+        {
+            driver = new ChromeDriver();
+            //A09
+            driver.Url = url;
+
+            //A10
+            DexcomTestPage page = new DexcomTestPage(driver);
+            page.TypeFirstName(fname);
+            page.TypeLastName(lname);
+            page.TypeTitle(title);
+            page.TypePhone(phone);
+            page.TypeBusiness(bus);
+            page.TypeAddressLn1(add1);
+            page.TypeCity(city);
+            page.TypeState("OR");
+            page.TypeZip(zip);
+            page.TermsBox.Click();
+            page.PrivacyBox.Click();
+            page.TypeEmail("abc");
+            page.SaveBtn.Click();
+            TakeScreenshot(driver, "A10", timestamp);
+
+            //A11
+            page.ClearAllFields();
+            page.TypeFirstName(fname);
+            page.TypeLastName(lname);
+            page.TypeTitle(title);
+            page.TypePhone(phone);
+            page.TypeBusiness(bus);
+            page.TypeAddressLn1(add1);
+            page.TypeCity(city);
+            page.TypeState("OR");
+            page.TypeZip(zip);
+            page.TermsBox.Click();
+            page.PrivacyBox.Click();
+            page.TypeEmail("a@bc");
+            page.SaveBtn.Click();
+            TakeScreenshot(driver, "A11", timestamp);
+
+            //A12
+            page.ClearAllFields();
+            page.TypeFirstName(fname);
+            page.TypeLastName(lname);
+            page.TypeTitle(title);
+            page.TypePhone(phone);
+            page.TypeBusiness(bus);
+            page.TypeAddressLn1(add1);
+            page.TypeCity(city);
+            page.TypeState("OR");
+            page.TypeZip(zip);
+            page.TermsBox.Click();
+            page.PrivacyBox.Click();
+            page.TypeEmail("a@b.c");
+            page.SaveBtn.Click();
+            TakeScreenshot(driver, "A12", timestamp);
+
+            driver.Close();
+
+        }
+
+        [Test]
+        [Category("UI")]
+        public void A13thruA15()
+        {
+            driver = new ChromeDriver();
+            //A13
+            driver.Url = url;
+
+            //A14
+            DexcomTestPage page = new DexcomTestPage(driver);
+
+            page.ClearAllFields();
+            page.TypeFirstName(fname);
+            page.TypeLastName(lname);
+            page.TypeEmail(email);
+            page.TypeTitle(title);
+            page.TypePhone(phone);
+            page.TypeBusiness(bus);
+            page.TypeAddressLn1(add1);
+            page.TypeCity(city);
+            page.TypeState("OR");            
+            page.TermsBox.Click();
+            page.PrivacyBox.Click();
+            page.TypeZip(zip4);
+
+            page.SaveBtn.Click();
+            TakeScreenshot(driver, "A14", timestamp);
+
+            page.ClearAllFields();
+            page.TypeFirstName(fname);
+            page.TypeLastName(lname);
+            page.TypeEmail(email);
+            page.TypeTitle(title);
+            page.TypePhone(phone);
+            page.TypeBusiness(bus);
+            page.TypeAddressLn1(add1);
+            page.TypeCity(city);
+            page.TypeState("OR");
+            page.TermsBox.Click();
+            page.PrivacyBox.Click();
+            page.TypeZip(zip24);
+
+            page.SaveBtn.Click();
+            TakeScreenshot(driver, "A15", timestamp);
+
+            driver.Close();
+        }
+
+
+           //Save a screenshot to the directoy defined by dir
+            protected void TakeScreenshot(IWebDriver driver, string stepName, string runtime)
         {
             
             Screenshot ss = ((ITakesScreenshot)driver).GetScreenshot();
-            //ss.SaveAsFile("C:/temp/" + stepName + ".png", ImageFormat.Png);
+            ss.SaveAsFile(dir + stepName +"_"+ runtime + ".png", ScreenshotImageFormat.Png);
 
         }
     }
--- _Data/_Models/_PageObjects/DexcomTestPageObjects.cs
+++ _Data/_Models/_PageObjects/DexcomTestPageObjects.cs
@@ -1,12 +1,131 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.UI;
 using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace DexcomCodeChallenge._Data._Models._PageObjects
 {
-    public class DexcomTestPageObjects 
+    public class DexcomTestPage
     {
-        IWebDriver driver;
+        IWebDriver _driver;
+
+        public DexcomTestPage(IWebDriver driver)
+        {
+            _driver = driver;            
+        }
+
+        public IWebElement FirstName => _driver.FindElement(By.Id("user_first_name"));
+        public IWebElement LastName => _driver.FindElement(By.Id("user_last_name"));
+        public IWebElement Suffix => _driver.FindElement(By.Id("user_suffix"));
+        public IWebElement Title => _driver.FindElement(By.Id("user_title"));
+        public IWebElement Email => _driver.FindElement(By.Id("user_email"));
+        public IWebElement Phone => _driver.FindElement(By.Id("user_phone_number"));
+        public IWebElement Business => _driver.FindElement(By.Id("user_business_name"));
+        public IWebElement AddressLn1 => _driver.FindElement(By.Id("user_address_1"));
+        public IWebElement AddressLn2 => _driver.FindElement(By.Id("user_address_2"));
+        public IWebElement City => _driver.FindElement(By.Id("user_city"));
+        public IWebElement State => _driver.FindElement(By.Id("user_state"));
+        public IWebElement Zip => _driver.FindElement(By.Id("user_zip"));
+        public IWebElement TermsBox => _driver.FindElement(By.Id("user_agree_1"));
+        public IWebElement PrivacyBox => _driver.FindElement(By.Id("user_agree_2"));
+        public IWebElement SaveBtn => _driver.FindElement(By.Name("commit"));
+
+        #region Fill Out Fields
+        public void TypeFirstName(string firstName)
+        {
+            FirstName.SendKeys(firstName);
+        }
+        public void TypeLastName(string lastName)
+        {
+            LastName.SendKeys(lastName);
+        }
+        public void TypeTitle(string title)
+        {
+            Title.SendKeys(title);
+        }
+        public void TypeEmail(string email)
+        {
+            Email.SendKeys(email);
+        }
+        public void TypePhone(string phone)
+        {
+            Phone.SendKeys(phone);
+        }
+        public void TypeBusiness(string business)
+        {
+            Business.SendKeys(business);
+        }
+        public void TypeAddressLn1(string addressLn1)
+        {
+            AddressLn1.SendKeys(addressLn1);
+        }
+        public void TypeAddressLn2(string addressLn2)
+        {
+            AddressLn2.SendKeys(addressLn2);
+        }
+        public void TypeCity(string city)
+        {
+            City.SendKeys(city);
+        }
+        public void TypeZip(string zip)
+        {
+            Zip.SendKeys(zip);
+        }
+      public void TypeState(string state)
+        {
+            State.SendKeys(state);
+        }
+
+    public void PickState(string state)
+        {
+            var selectState = new SelectElement(State);
+            selectState.SelectByText(state);                
+        }
+        public void TypeSuffix(string suffix)
+        {
+            Suffix.SendKeys(suffix);
+        }
+
+        public void PickSuffix(string suffix)
+        {
+            var selectSuffix = new SelectElement(Suffix);
+            selectSuffix.SelectByText(suffix);
+        }
+
+        public void Check(IWebElement checkbox, Boolean check)
+        {
+            if(!check && checkbox.Selected)
+            {
+                checkbox.Click();
+            }
+        }
+
+        public void Uncheck(IWebElement checkbox, Boolean check)
+        {
+            if(check && checkbox.Selected)
+            {
+                checkbox.Click();
+            }
+        }
+        #endregion
+        public void ClearAllFields()
+        {
+            FirstName.Clear();
+            LastName.Clear();
+            PickSuffix("");
+            Title.Clear();
+            Email.Clear();
+            Phone.Clear();
+            Business.Clear();
+            AddressLn1.Clear();
+            AddressLn2.Clear();
+            City.Clear();
+            PickState("");
+            Zip.Clear();
+            Uncheck(TermsBox, true);
+            Uncheck(PrivacyBox, true);            
+        }
+
     }
 }
